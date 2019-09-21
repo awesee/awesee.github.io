a737d96ea6c8b8125b24b9c6ed0eb9d3c0cf0af0
openset <openset.wang@gmail.com>
2019-09-21 21:36:27 +0800

    Add: 百度自动推送

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,3 +3,10 @@
         $(".page__footer").css("position", "absolute");
     });
 })(jQuery);
+// 百度自动推送工具
+(function () {
+    var bp = document.createElement('script');
+    bp.src = '//zz.bdstatic.com/linksubmit/push.js';
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
