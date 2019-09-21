39f786afb433c45b4ea268bb422dd61d2a298e33
openset <openset.wang@gmail.com>
2019-09-21 22:17:10 +0800

    Add: async

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,6 +6,8 @@
 // 百度自动推送工具
 (function () {
     var bp = document.createElement('script');
+    bp.type = 'text/javascript';
+    bp.async = true;
     bp.src = '//zz.bdstatic.com/linksubmit/push.js';
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
